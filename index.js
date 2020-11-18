ae6d903554c7b26aa7f0311e3847f1600675dee7
maidersonn <maidersonn@gmail.com>
2020-11-18 23:09:03 +0100

    alta de nuevos animales

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-
-
 let animalsToAddopt = [
-    { animal : "dog", name : "Zira",},
-    { animal : "dog", name : "Atxa",},
-    { animal : "cat", name : "Mixi",},
-    { animal : "cat", name : "Poli",}
+  { animal: "dog", name: "Zira" },
+  { animal: "dog", name: "Atxa" },
+  { animal: "cat", name: "Mixi" },
+  { animal: "cat", name: "Poli" },
 ];
 
+function addAnimals() {
+  let nameToAdd = document.getElementById("nameInput").value;
+  let animalNodeList = document.getElementsByName("animal");
+  let animalToAdd = Array.from(animalNodeList).filter(
+    (animal) => animal.checked === true
+  )[0].value;
+  let animal = {
+    animal: animalToAdd,
+    name: nameToAdd,
+  };
+  animalsToAddopt.push(animal);
+  showList();
+}
 
-function showList(){
-    let list = document.getElementById("animalList");
-    animalsToAddopt.forEach(function(data){
-        let linew = document.createElement("li");
-        let content = document.createTextNode(data.animal + " " + data.name);
-        list.appendChild(linew);
-        linew.appendChild(content);
-    })
+function showList() {
+  let list = document.getElementById("animalList");
+  list.innerHTML = "";
+  animalsToAddopt.forEach(function (data) {
+    let linew = document.createElement("li");
+    let content = document.createTextNode(data.animal + " " + data.name);
+    list.appendChild(linew);
+    linew.appendChild(content);
+  });
 }
 showList();
 
-function addAnimals (animal, name) {
-    let nameToAdd = document.getElementById("nameIput").value;
-}
-
-
+document
+  .getElementById("addAnimalButton")
+  .addEventListener("click", addAnimals);
